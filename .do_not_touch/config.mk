7ebbcb698fdb589a5981e125a6ac2041b12bcd8a
Frédéric Hardy <frederic.hardy@mageekbox.net>
2021-03-05 19:46:29 +0100

    Update vendors.

--- .do_not_touch/config.mk
+++ .do_not_touch/config.mk
@@ -1,1 +1,1 @@
-INSTALL_DEPENDENCIES:= bin/php bin/composer git Makefile .do_not_touch/Makefile VERSION LICENCE README.md src/. tests/units github
+INSTALL_DEPENDENCIES:=  bin/php bin/composer git Makefile .do_not_touch/Makefile VERSION LICENCE README.md CHANGELOG.md src/. tests/units github
